84b2bba9a7137fe645e61d8f718456aac43dc72a
Adam Lindgren <adam@adamlindgren.com>
2022-04-22 22:34:05 -0400

    npm installed axios, react-router-dom, use-local-storage-state. Implimented first route to Landing

--- charitable-tracker/src/App.js
+++ charitable-tracker/src/App.js
@@ -1,11 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
+// import useLocalStorageState from 'use-local-storage-state'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  // Link,
+  // useNavigate,
+} from 'react-router-dom';
+// import axios from 'axios'
+import Landing from './components/Landing';
 
 function App() {
   console.log('Hello, World!');
   return (
     <>
-      <h1>Hello, World!</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+        </Routes>
+      </Router>
     </>
   );
 }
