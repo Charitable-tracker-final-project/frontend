888cda18cb4d8490f14bbd1674328b7867f4c0a6
Adam Lindgren <adam@adamlindgren.com>
2022-04-24 14:27:09 -0400

    added navbar

--- charitable-tracker/src/components/Home.js
+++ charitable-tracker/src/components/Home.js
@@ -1,8 +1,51 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Link } from 'react-router-dom';
+import logo from '../images/logo192.png';
+
 export default function Home({ handleLogOut }) {
   return (
     <>
-      <h1>Logged In</h1>
-      <button onClick={handleLogOut}>Log Out</button>
+      <nav
+        className='navbar pr-2 pt-1 has-background-link-light'
+        role='navigation'
+        aria-label='main navigation'
+      >
+        <div className='navbar-brand'>
+          <Link className='navbar-item' to='/'>
+            <img src={logo} alt='webiste logo' height={2000} className='logo' />
+          </Link>
+          <a
+            role='button'
+            className='navbar-burger'
+            aria-label='menu'
+            aria-expanded='false'
+            data-target='charitableNavbar'
+          >
+            <span aria-hidden='true' />
+            <span aria-hidden='true' />
+            <span aria-hidden='true' />
+          </a>
+        </div>
+
+        <div id='charitableNavbar' className='navbar-menu'>
+          <div className='navbar-start'>
+            <Link to='/new/donation' className='navbar-item'>
+              Enter Donations
+            </Link>
+            <Link to='/new/volunteer-hours' className='navbar-item'>
+              Enter Volunteer Hours
+            </Link>
+          </div>
+          <div className='navbar-end'>
+            <Link className='navbar-item' to='/results'>
+              Results
+            </Link>
+            <a className='button is-warning mt-1' onClick={handleLogOut}>
+              Log Out
+            </a>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
